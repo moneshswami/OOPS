0c85b4d0b7923841b26a6f588b748a9ba60a7556
moneshswami <120643525+moneshswami@users.noreply.github.com>
2024-05-01 23:51:21 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,59 +1,59 @@
-#include&lt;iostream&gt;
-#include&lt;stdio.h&gt;
-#include&lt;conio.h&gt;
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
 using namespace std;
-class person
-{
+
+class person {
 public:
-char name[10];
-int code;
-void getp()
-{
-cout&lt;&lt;&quot;Enter Name : &quot;;
-cin&gt;&gt;name;
-cout&lt;&lt;&quot;\nEnter Code : &quot;;
-cin&gt;&gt;code;
-}
-};
-class account : virtual public person
-{
-public:
-double pay;
-void getac()
-{
-cout&lt;&lt;&quot;\nEnter Salary : &quot;;
-cin&gt;&gt;pay;
-}
+    char name[10];
+    int code;
+
+    void getp() {
+        cout << "Enter Name: ";
+        cin >> name;
+        cout << "\nEnter Code: ";
+        cin >> code;
+    }
 };
 
-class admin : virtual public person
-{
+class account : virtual public person {
 public:
-double exp;
-void getad()
-{
-cout&lt;&lt;&quot;\nEnter Experience : &quot;;
-cin&gt;&gt;exp;
-}
+    double pay;
+
+    void getac() {
+        cout << "\nEnter Salary: ";
+        cin >> pay;
+    }
 };
-class employee : public account, public admin
-{
+
+class admin : virtual public person {
 public:
-void disp()
-{
-cout&lt;&lt;&quot;\n=========================\nName\t\t: &quot;&lt;&lt;name;
-cout&lt;&lt;&quot;\nCode\t\t: &quot;&lt;&lt;code;
-cout&lt;&lt;&quot;\nSalary\t\t: &quot;&lt;&lt;pay;
-cout&lt;&lt;&quot;\nExperience\t: &quot;&lt;&lt;exp;
-cout&lt;&lt;&quot;\n=========================&quot;;
-}
+    double exp;
+
+    void getad() {
+        cout << "\nEnter Experience: ";
+        cin >> exp;
+    }
 };
-int main()
-{
-employee e1;
-e1.getp();
-e1.getac();
-e1.getad();
-e1.disp();
-getch();
+
+class employee : public account, public admin {
+public:
+    void disp() {
+        cout << "\n=========================\n";
+        cout << "Name\t\t: " << name;
+        cout << "\nCode\t\t: " << code;
+        cout << "\nSalary\t\t: " << pay;
+        cout << "\nExperience\t: " << exp;
+        cout << "\n=========================";
+    }
+};
+
+int main() {
+    employee e1;
+    e1.getp();
+    e1.getac();
+    e1.getad();
+    e1.disp();
+    getch();
+    return 0;
 }
