c56fcf8941264afc55dee900ec562100781bc113
moneshswami <120643525+moneshswami@users.noreply.github.com>
2024-05-01 23:44:01 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,100 +1,80 @@
-#include&lt;iostream&gt;
+#include <iostream>
 using namespace std;
 
-class MAT
-{
-int a[2][2];
+class MAT {
+    int a[2][2];
 
 public:
-void accept()
-{
-cout&lt;&lt;&quot;\n\n Enter 4 element : &quot;;
-for(int i=0;i&lt;2;i++)
-{
-for(int j=0;j&lt;2;j++)
+    void accept() {
+        cout << "\n\n Enter 4 elements: ";
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++)
+                cin >> a[i][j];
+        }
+    }
 
-cin&gt;&gt;a[i][j];
-}
-}
+    void display() {
+        for(int i = 0; i < 2; i++) {
+            cout << endl;
+            for(int j = 0; j < 2; j++)
+                cout << " " << a[i][j];
+        }
+    }
 
-void display()
-{
-for(int i=0;i&lt;2;i++)
-{
-cout&lt;&lt;endl;
-for(int j=0;j&lt;2;j++)
+    MAT operator+(MAT M2) {
+        MAT M3;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                M3.a[i][j] = a[i][j] + M2.a[i][j];
+            }
+        }
+        return M3;
+    }
 
-cout&lt;&lt;&quot; &quot;&lt;&lt;a[i][j];
-}
-}
+    MAT operator-(MAT M2) {
+        MAT M3;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                M3.a[i][j] = a[i][j] - M2.a[i][j];
+            }
+        }
+        return M3;
+    }
 
-MAT operator+(MAT M2)
-{
-MAT M3;
-for(int i=0;i&lt;2;i++)
-{
-for(int j=0;j&lt;2;j++)
-{
-M3.a[i][j]=a[i][j]+M2.a[i][j];
-}
-}
-return M3;
-}
-
-MAT operator-(MAT M2)
-{
-MAT M3;
-for(int i=0;i&lt;2;i++)
-{
-for(int j=0;j&lt;2;j++)
-{
-M3.a[i][j]=a[i][j]-M2.a[i][j];
-}
-}
-return M3;
-}
-
-MAT operator*(MAT M2)
-{
-MAT M3;
-
-for(int i=0;i&lt;2;i++)
-{
-for(int j=0;j&lt;2;j++)
-{
-M3.a[i][j]=0;
-for(int k=0;k&lt;2;k++)
-{
-M3.a[i][j]=(a[i][k]*M2.a[k][j])+M3.a[i][j];
-}
-}
-}
-
-return M3;
-}
+    MAT operator*(MAT M2) {
+        MAT M3;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                M3.a[i][j] = 0;
+                for(int k = 0; k < 2; k++) {
+                    M3.a[i][j] = (a[i][k] * M2.a[k][j]) + M3.a[i][j];
+                }
+            }
+        }
+        return M3;
+    }
 };
 
-int main()
-{
-MAT M1,M2,M3;
+int main() {
+    MAT M1, M2, M3;
 
-cout&lt;&lt;&quot;\n\n Enter Matrix M1 value: &quot;;
-M1.accept();
+    cout << "\n\n Enter Matrix M1 value: ";
+    M1.accept();
 
-cout&lt;&lt;&quot;\n\n Enter Matrix M2 value: &quot;;
-M2.accept();
+    cout << "\n\n Enter Matrix M2 value: ";
+    M2.accept();
 
-M3=M1+M2;
-cout&lt;&lt;&quot;\n\n Addition of M1+M2 : &quot;;
-M3.display();
+    M3 = M1 + M2;
+    cout << "\n\n Addition of M1 + M2: ";
+    M3.display();
 
-M3=M1-M2;
-cout&lt;&lt;&quot;\n\n Subtrachtion of M1-M2 : &quot;;
-M3.display();
+    M3 = M1 - M2;
+    cout << "\n\n Subtraction of M1 - M2: ";
+    M3.display();
 
-M3=M1*M2;
-cout&lt;&lt;&quot;\n\n Multiplication of M1*M2 : &quot;;
-M3.display();
+    M3 = M1 * M2;
+    cout << "\n\n Multiplication of M1 * M2: ";
+    M3.display();
 
-return 0;
+    return 0;
 }
