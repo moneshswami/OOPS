368b9fbc5095445dae1058854857f4cd9e11cb64
moneshswami <120643525+moneshswami@users.noreply.github.com>
2024-05-01 23:49:26 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,22 +1,24 @@
-#include&lt;iostream&gt;
+#include <iostream>
 using namespace std;
-float area(float,float);
+
+float area(float, float);
 float area(float);
-int main()
-{
-float r,B,H;
-cout&lt;&lt;&quot;Enter base and height of triangle:&quot;;
-cin&gt;&gt;B&gt;&gt;H;
-cout&lt;&lt;&quot;Enter radius of circle:&quot;;
-cin&gt;&gt;r;
-cout&lt;&lt;&quot;\nArea of triangle is &quot;&lt;&lt;area(B,H);
-cout&lt;&lt;&quot;\nArea of circle is &quot;&lt;&lt;area(r);
+
+int main() {
+    float r, B, H;
+    cout << "Enter base and height of triangle: ";
+    cin >> B >> H;
+    cout << "Enter radius of circle: ";
+    cin >> r;
+    cout << "\nArea of triangle is " << area(B, H);
+    cout << "\nArea of circle is " << area(r);
+    return 0;
 }
-float area(float B,float H)
-{
-return((B*H)/2);
+
+float area(float B, float H) {
+    return ((B * H) / 2);
 }
-float area(float r)
-{
-return(3.14*r*r);
+
+float area(float r) {
+    return (3.14 * r * r);
 }
